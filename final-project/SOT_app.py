66870f9167356c65b17e02f4e4dd5067dd47ac07
Jesse_Carter <jessetcarter@hotmail.com>
2026-06-14 22:29:03 -0600

    updated SOT_app and SOT_db functions

--- final-project/SOT_app.py
+++ final-project/SOT_app.py
@@ -1,6 +1,15 @@
 import streamlit as st
 
-from SOT_db import get_rank, get_TSC, get_workcenter, get_personnel
+from SOT_db import (
+  get_rank, get_TSC, 
+  get_workcenter, 
+  get_personnel, 
+  get_training_status_by_WC, 
+  get_MTP,
+  get_MTL_by_WC,
+  get_ITP_by_DOD_id,
+  get_CDC_by_DOD_id
+)
 
 st.set_page_config(
   page_title="Database-Backed AI Application",
@@ -18,22 +27,54 @@ st.write(
 )
 
 try:
-  st.subheader("Personnel")
-  items = get_personnel()
-  st.dataframe(items, use_container_width=True)
-
   st.subheader("Enlisted Ranks")
   items = get_rank()
-  st.dataframe(items, use_container_width=True)
-
-  st.subheader("Training Status Codes")
-  items = get_TSC()
   st.dataframe(items, use_container_width=True)
 
   st.subheader("Work Centers")
   items = get_workcenter()
   st.dataframe(items, use_container_width=True)
 
+  st.subheader("Training Status Codes")
+  items = get_TSC()
+  st.dataframe(items, use_container_width=True)
+
+  st.subheader("Personnel")
+  items = get_personnel()
+  st.dataframe(items, use_container_width=True)
+
+  st.subheader("Master Training Plans")
+  items = get_MTP()
+  st.dataframe(items, use_container_width=True)
+
+  st.subheader("Master Task List by Work Center")
+  items = get_MTL_by_WC("EMT")
+  st.dataframe(items, use_container_width=True)
+
+  st.subheader("Individual Training Plans")
+  items = get_ITP_by_DOD_id("863287637")
+  st.dataframe(items, use_container_width=True)
+
+  st.subheader("Career Development Course Status")
+  items = get_CDC_by_DOD_id("863287637")
+  st.dataframe(items, use_container_width=True)
+
+  st.subheader("Training Status by Work Center")
+  items = get_training_status_by_WC()
+  st.dataframe(items, use_container_width=True)
+
 except Exception as error:
   st.error("The application could not load database records.")
   st.exception(error)
+
+if __name__ == "__main__":
+  print("Testing database access functions...\n")
+
+  print("Work Centers:")
+  print(get_workcenter())
+
+  print("Personnel:")
+  print(get_personnel())
+
+  print("Amn Vance's ITP:")
+  print(get_ITP_by_DOD_id("863287637"))
--- final-project/SOT_db.py
+++ final-project/SOT_db.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 import pandas as pd
 
-DATABASE_PATH = Path("data/statusOfTraining.db")
+DATABASE_PATH = Path("data/statusOfTrainingV2.db")
 
 def get_connection() -> sqlite3.Connection:
   """Return a connection to the local SQLite database."""
@@ -29,10 +29,9 @@ def get_TSC() -> pd.DataFrame:
     SELECT 
     TSC, 
     defined AS 'Definition'
-    FROM training_status_code
-    WHERE TSC = 'A' OR TSC = 'B' OR TSC = 'C' OR TSC = 'R';
+    FROM training_status_code;
   """
-
+    #WHERE TSC = 'A' OR TSC = 'B' OR TSC = 'C' OR TSC = 'R';
   with get_connection() as conn:
     return pd.read_sql_query(query, conn)
   
@@ -59,12 +58,118 @@ def get_personnel() -> pd.DataFrame:
       AFSC,
       TSC,
       supervisor_name AS 'Supervisor',
-      WC_id AS 'Workcenter',
+      office_symbol AS 'Workcenter',
       date_of_enlistment AS 'DOE',
       date_of_rank AS 'DOR',
+      date_arrived_station AS 'DAS',
       date_of_separation AS 'DOS'
     FROM personnel;
   """
 
   with get_connection() as conn:
     return pd.read_sql_query(query, conn)
+  
+def get_training_status_by_WC() -> pd.DataFrame:
+  """Return all personnel records by Training Status Code."""
+  query = """ 
+    SELECT
+      office_symbol AS 'Workcenter',
+      TSC,
+      COUNT(TSC)
+    FROM personnel
+    GROUP BY office_symbol, TSC;
+  """
+
+  with get_connection() as conn:
+    return pd.read_sql_query(query, conn)
+  
+def get_MTP() -> pd.DataFrame:
+  """Return all records from the workcenter table."""
+  query = """ 
+    SELECT 
+    plan_title AS 'MTP Title', 
+    AFSC
+    FROM master_training_plan;
+  """
+
+  with get_connection() as conn:
+    return pd.read_sql_query(query, conn)
+  
+def get_MTL_by_WC(office_symbol: str) -> pd.DataFrame:
+  """Return all personnel records by Training Status Code."""
+  query = """ 
+    SELECT 
+      w.office_symbol AS "Work Center",
+      mtp.plan_title AS "MTP Title",
+      mtp.AFSC,
+      mtl.task_id AS "Task ID",
+      mtl.skill_lvl AS "Skill Lvl",
+      mtl.task_name AS "Task"
+    FROM workcenter AS w
+    JOIN master_training_plan AS mtp ON w.WC_id = mtp.WC_id
+    JOIN master_task_list AS mtl ON mtp.MTP_id = mtl.MTP_id
+    WHERE w.office_symbol = ?;
+  """
+
+  with get_connection() as conn:
+    return pd.read_sql_query(query, conn, params=(office_symbol,))
+  
+def get_ITP_by_DOD_id(DOD_id: str) -> pd.DataFrame:
+  """Return all personnel records by Training Status Code."""
+  query = """ 
+    SELECT 
+      p.rank AS "Rank",
+      p.last_name AS "Last Name",
+      p.first_name AS "First",
+      p.office_symbol AS "Work Center",
+      itp.skill_lvl AS "Skill Lvl",
+      itp.task_id AS "Task ID",
+      itp.task_name AS "Task",
+      itp.task_start_date AS "Start Date",
+      itp.is_task_complete,
+      CASE
+        WHEN itp.is_task_complete = 0 THEN "No"
+        ELSE "Yes"
+      END AS "Complete?"
+    FROM personnel AS p
+    JOIN individual_training_plan AS itp ON p.DOD_id = itp.DOD_id
+    WHERE p.DOD_id = ?;
+  """
+
+  with get_connection() as conn:
+    return pd.read_sql_query(query, conn, params=(DOD_id,))
+  
+
+def get_CDC_by_DOD_id(DOD_id: str) -> pd.DataFrame:
+  """Return all personnel records by Training Status Code."""
+  query = """ 
+    SELECT 
+      p.rank AS "Rank",
+      p.last_name AS "Last Name",
+      p.first_name AS "First",
+      p.office_symbol AS "Work Center",
+      p.supervisor_name AS "Supervisor",
+      cdc.CDC_version AS "CDC Version",
+      cdc.order_date AS "Order Date",
+      cdc.is_received, 
+      CASE
+        WHEN cdc.is_received = 0 THEN "No"
+        ELSE "Yes"
+      END AS "Received?",
+      cdc.is_EOC_scheduled,
+      CASE
+        WHEN cdc.is_EOC_scheduled = 0 THEN "No"
+        ELSE "Yes"
+      END AS "Test Scheduled?",
+      cdc.is_completed,
+      CASE
+        WHEN cdc.is_completed = 0 THEN "No"
+        ELSE "Yes"
+      END AS "Complete?"
+    FROM personnel AS p
+    JOIN career_dev_courses AS cdc ON p.DOD_id = cdc.DOD_id
+    WHERE p.DOD_id = ?;
+  """
+
+  with get_connection() as conn:
+    return pd.read_sql_query(query, conn, params=(DOD_id,))
